b964b45b2bba72493567b689310307d95f1c52c5
Maksim Prokopev <maksim.prokopev@kiwi.com>
2025-12-04 13:05:25 +0100

    added logs

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 """Example of flask main file."""
 from flask import Flask
 import boto3
+import logging
+from logging.handlers import FileHandler
 
 app = Flask(__name__)
+
+# Configure file logging
+file_handler = FileHandler('/tmp/application.log')
+file_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+app.logger.addHandler(file_handler)
+app.logger.setLevel(logging.INFO)
 
 
 @app.route('/api/hello')
@@ -15,7 +25,9 @@ def hello_world():
 def root():
     """Returns Hello, EDP!"""
     responce = boto3.client("s3").get_object(Bucket='kuberocketci-applications-data', Key='cmtr-if35mrq2/data.txt')
-    answer = f"{'content': {{ responce }} }"
+    app.logger.info('Accessed the index page.')
+    app.logger.info(responce)
+    answer = "{'content': 'hello' }"
     return answer
 
 if __name__ == '__main__':

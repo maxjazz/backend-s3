933c978c74c51cecc3a7c9e902614ad37b252d27
Maksim Prokopev <maksim.prokopev@kiwi.com>
2025-12-04 13:41:30 +0100

    Update body

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def hello_world():
 @app.route('/')
 def root():
     """Returns Hello, EDP!"""
-    responce = boto3.client("s3").get_object(Bucket='kuberocketci-applications-data', Key='cmtr-if35mrq2/data.txt')
+    response = boto3.client("s3").get_object(Bucket='kuberocketci-applications-data', Key='cmtr-if35mrq2/data.txt')
     app.logger.info('Accessed the index page.')
     body = response["Body"].read().decode("utf-8") 
     app.logger.info(body)
